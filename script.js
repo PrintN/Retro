86159200584db9095e550efb0d456dee4b22ff5b
PrintN <127101769+PrintN@users.noreply.github.com>
2025-10-22 12:58:29 +0200

    feat: Add pagination

--- script.js
+++ script.js
@@ -180,6 +180,54 @@ async function fetchGuestbookEntries() {
     tr.appendChild(td);
     entriesList.appendChild(tr);
   });
+
+  // Pagination
+  const guestbook = document.querySelector('.guestbook'); 
+  const itemsPerPage = 7;
+  let currentPage = 0;
+  const items = Array.from(guestbook.getElementsByTagName('tr')).slice(1);
+
+  function showPage(page) {
+    const startIndex = page * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    items.forEach((item, index) => {
+      item.classList.toggle('hidden', index < startIndex || index >= endIndex);
+    });
+    updateActiveButtonStates();
+  }
+
+  function createPageButtons() {
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+    const paginationContainer = document.createElement('div');
+    const paginationDiv = document.body.appendChild(paginationContainer);
+    paginationContainer.classList.add('pagination');
+
+    for (let i = 0; i < totalPages; i++) {
+      const pageButton = document.createElement('button');
+      pageButton.textContent = i + 1;
+      pageButton.addEventListener('click', () => {
+        currentPage = i;
+        showPage(currentPage);
+        updateActiveButtonStates();
+      });
+
+        guestbook.appendChild(paginationContainer);
+        paginationDiv.appendChild(pageButton);
+      }
+  }
+
+  function updateActiveButtonStates() {
+    const pageButtons = document.querySelectorAll('.pagination button');
+    pageButtons.forEach((button, index) => {
+      if (index === currentPage) {
+        button.classList.add('active');
+      } else {
+        button.classList.remove('active');
+      }
+    });
+  }
+  createPageButtons();
+  showPage(currentPage);
 }
 
 supabase
